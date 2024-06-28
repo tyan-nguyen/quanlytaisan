158f488dbeeb9c29c97d47e1c0252b784e708f4b
Nvtan <nguyenvantyan@gmail.com>
2024-06-28 09:13:10 +0700

    update

---
+++ migrations/m240628_125934_alter_table_ts_ke_hoach_bao_tri.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240507_125934_create_table_ts_ke_hoach_bao_tri
+ */
+class m240628_125934_alter_table_ts_ke_hoach_bao_tri extends Migration
+{
+    
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->addColumn('tan_suat', $this->integer()->after('ky_bao_tri'));
+    }
+    
+    public function down()
+    {
+        
+    }
+    
+}
