02ae126da68242737e21a0bcf71825d898193a39
Nvtan <nguyenvantyan@gmail.com>
2024-06-28 09:28:52 +0700

    update

--- migrations/m240628_125934_alter_table_ts_ke_hoach_bao_tri.php
+++ migrations/m240628_125934_alter_table_ts_ke_hoach_bao_tri.php
@@ -11,12 +11,12 @@ class m240628_125934_alter_table_ts_ke_hoach_bao_tri extends Migration
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
-        $this->addColumn('tan_suat', 'integer AFTER `ky_bao_tri`');
+        $this->addColumn('ts_ke_hoach_bao_tri','tan_suat', 'integer AFTER `ky_bao_tri`');
     }
     
     public function down()
     {
-        $this->dropColumn('tan_suat');
+        $this->dropColumn('ts_ke_hoach_bao_tri', 'tan_suat');
     }
     
 }
