f4f3469481e8c634d6ab2f06c8844c93b1a4c67e
hoangphi.th <hoangphi.th@gmail.com>
2024-07-16 22:34:20 +0700

    fix issue bộ phận sửa chữa không lưu được

--- modules/bophan/models/BoPhanBase.php
+++ modules/bophan/models/BoPhanBase.php
@@ -18,7 +18,7 @@ class BoPhanBase extends \app\models\TsBoPhan
     {
         return [
             [['ma_bo_phan', 'ten_bo_phan'], 'required'],
-            [['truc_thuoc', 'la_dv_quan_ly', 'la_dv_su_dung', 'la_dv_bao_tri', 'la_dv_van_tai', 'la_dv_mua_hang', 'la_dv_quan_ly_kho', 'la_trung_tam_chi_phi', 'id_kho_vat_tu', 'id_kho_phe_lieu', 'id_kho_thanh_pham', 'nguoi_tao'], 'integer'],
+            [['truc_thuoc', 'la_dv_quan_ly', 'la_dv_su_dung', 'la_dv_bao_tri', 'la_dv_van_tai', 'la_dv_mua_hang', 'la_dv_quan_ly_kho', 'la_trung_tam_chi_phi', 'id_kho_vat_tu', 'id_kho_phe_lieu', 'id_kho_thanh_pham', 'nguoi_tao','la_dv_sua_chua'], 'integer'],
             [['thoi_gian_tao'], 'safe'],
             [['ma_bo_phan'], 'string', 'max' => 20],
             [['ten_bo_phan'], 'string', 'max' => 255],
