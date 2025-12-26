a7f6a5fb263e55442c0575e4fa62981ffb3ebfd5
Nvtan <nguyenvantyan@gmail.com>
2025-12-26 14:02:31 +0700

    qr

--- modules/taisan/views/qr/_print_qr.php
+++ modules/taisan/views/qr/_print_qr.php
@@ -5,13 +5,17 @@ use yii\helpers\Html;
 <div class="wrap">
     <div class="div-left">
         <table>
-        <tr>
-        	<td><?= Html::img($model->qrCode, ['class'=>'img']) ?></td>
-        	<td class="title"><?= $model->ten_thiet_bi ?></td>
-        </tr>
-        <tr>
-        <td colspan="2" align="center" class="title-2">DNTN SX-TM Nguyễn Trình</td>
-        </tr>
+            <tr>
+            	<td colspan="2" align="center" class="title">DNTN SX-TM Nguyễn Trình</td>
+            </tr>
+            <tr>
+            	<td><?= Html::img($model->qrCode, ['class'=>'img']) ?></td>
+            	<!-- <td class="title"><?= $model->ten_thiet_bi ?></td> -->
+            	<td><?= Html::img('assets/images/brand/favicon.png', ['class'=>'img']) ?></td>
+            </tr>     
+            <tr>
+            	<td colspan="2" align="center" class="title-2"><?= $model->ten_thiet_bi ?></td>
+            </tr>   
         </table>
     </div>
     <!-- 
