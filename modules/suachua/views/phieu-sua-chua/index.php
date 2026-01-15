9f82d6cd05ab7f6097019b467e9d390a5ee00b74
Nvtan <nguyenvantyan@gmail.com>
2026-01-15 23:18:45 +0700

    cap nhat phieu sua chua

--- modules/suachua/views/phieu-sua-chua/index.php
+++ modules/suachua/views/phieu-sua-chua/index.php
@@ -51,7 +51,7 @@ Yii::$app->params['showExport'] = true;
             'condensed' => true,
             'responsive' => true,
             'panelHeadingTemplate'=>'{title}',
-            'panelFooterTemplate'=>'{summary}',
+            'panelFooterTemplate'=>'<div class="row"><div class="col-md-8">{pager}</div><div class="col-md-4">{summary}</div></div>',
             'summary'=>'Hiển thị dữ liệu {count}/{totalCount}, Trang {page}/{pageCount}',          
             'panel' => [
                 //'type' => 'primary', 
